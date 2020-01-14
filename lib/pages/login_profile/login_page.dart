aae220e81dd4c006e60fa68eb7a8b1f511a63e63
Tanishq <tanishqporwar22@gmail.com>
2020-01-14 13:37:49 +0530

    added qr code and scanner to the profile page

--- lib/pages/login_profile/login_page.dart
+++ lib/pages/login_profile/login_page.dart
@@ -13,76 +13,76 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        // gives a custom gradient color to the page
-        // jus remove this container to give it the default theme color
-        Container(
-          height: double.infinity,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0xFF222831),
-                Color(0xFF222831),
-                Color(0xFF25292e),
-                Color(0xFF28292b),
-              ],
-              stops: [0.05, 0.4, 0.7, 0.9],
-            ),
+    return Stack(children: <Widget>[
+      // gives a custom gradient color to the page
+      // jus remove this container to give it the default theme color
+      Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF222831),
+              Color(0xFF222831),
+              Color(0xFF25292e),
+              Color(0xFF28292b),
+            ],
+            stops: [0.05, 0.4, 0.7, 0.9],
           ),
         ),
+      ),
 
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
+      Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          // gap
+          SizedBox(
+            height: 10,
+          ),
 
-          children: <Widget>[
-            // gap
-            SizedBox(height: 10,),
-
-            // the small horizontal bar which indicates the location 
-            // to pull the sheet from
-            Container(
-              height: 5,
-              width: 30,
-              decoration: BoxDecoration(
+          // the small horizontal bar which indicates the location
+          // to pull the sheet from
+          Container(
+            height: 5,
+            width: 30,
+            decoration: BoxDecoration(
                 color: Colors.grey,
-                borderRadius: BorderRadius.all(Radius.circular(20))
-              ),
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+          ),
+
+          // gap
+          SizedBox(
+            height: 7,
+          ),
+
+          Expanded(
+            child: LoginWidget(
+              // widget to display when the login process is on going
+              onLoading: LoadingWidget(),
+
+              // widget to display when the login process was successful
+              onSuccess: ProfilePage(),
+
+              // the login form
+              loginForm: LoginForm(),
+
+              // callback to execute when an error occurs during the
+              // authentication process
+              onError: (BuildContext context, Exception e) {
+                Scaffold.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('${e.toString()}'),
+                    backgroundColor: Colors.red,
+                  ),
+                );
+              },
             ),
-
-            // gap
-            SizedBox(height: 7,),
-
-            Expanded(
-              child: LoginWidget(
-                // widget to display when the login process is on going
-                onLoading: LoadingWidget(),
-
-                // widget to display when the login process was successful
-                onSuccess: ProfilePage(),
-
-                // the login form
-                loginForm: LoginForm(),
-
-                // callback to execute when an error occurs during the
-                // authentication process
-                onError: (BuildContext context, Exception e) {
-                  Scaffold.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('${e.toString()}'),
-                      backgroundColor: Colors.red,
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ]
-    );
+          ),
+        ],
+      ),
+    ]);
   }
 }
--- lib/pages/login_profile/profile_page.dart
+++ lib/pages/login_profile/profile_page.dart
@@ -1,22 +1,70 @@
 import 'package:dwimay_backend/dwimay_backend.dart';
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+import 'dart:async';
+import 'package:flutter/services.dart';
 
 /// The profile page. The user logs in using the profile page.
 /// The details of the user and the QR code for entering will
 /// be present after logging in.
 class ProfilePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+    final message = User.instance.getEmailID();
+
+    Future<ui.Image> _loadOverlayImage() async {
+      final completer = Completer<ui.Image>();
+      final byteData = await rootBundle.load('assets/images/talk.png');
+      ui.decodeImageFromList(byteData.buffer.asUint8List(), completer.complete);
+      return completer.future;
+    }
+
+    final qrFutureBuilder = FutureBuilder(
+      future: _loadOverlayImage(),
+      builder: (ctx, snapshot) {
+        final size = 280.0;
+        if (!snapshot.hasData) {
+          return Container(width: size, height: size);
+        }
+        return CustomPaint(
+          size: Size.square(size),
+          painter: QrPainter(
+            data: message,
+            version: QrVersions.auto,
+            color: Colors.black,
+            emptyColor: Color(0x00),
+            // size: 320.0,
+            embeddedImage: snapshot.data,
+            embeddedImageStyle: QrEmbeddedImageStyle(
+              size: Size.square(60),
+            ),
+          ),
+        );
+      },
+    );
+
+    final level = User.instance.getClearanceLevel();
+
+    final _buildQrReader = RaisedButton(
+      onPressed: () async {
+        String res = await QrScanner.scan();
+      },
+      child: Text("Scan"),
+    );
+
+    final pass = (level != 0) ? qrFutureBuilder : _buildQrReader;
+
     return Center(
       child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            pass,
+            SizedBox(height: 20),
             Text(
               "Profile page",
               style: Theme.of(context).textTheme.title.copyWith(
-                color: Colors.white,
-              ),
+                    color: Colors.white,
+                  ),
             ),
             SizedBox(
               height: 20,
