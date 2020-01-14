f9a839631ec7f3f483216caa0700dfcc12ea9e96
firekind <shyamant.achar@gmail.com>
2020-01-14 20:47:10 +0530

    Added announcements page, tweaked about page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:dwimay/pages/main/main_page.dart';
 import 'package:dwimay/theme_data.dart';
-import 'package:dwimay_backend/dwimay_backend.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -16,21 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: dwimayTheme,
-      home: NotificationProvider(
-        // configuring callback to run when notification occurs when the
-        // app is in foreground.
-        onMessage: (BuildContext context, Map<String, dynamic> message) {
-          Scaffold.of(context).showSnackBar(
-            SnackBar(
-              content: message['body'],
-            )
-          );
-        },
-        
-        // rest of the app
-        child: MainPage(),
-      ),
-
+      home: MainPage(),
       debugShowCheckedModeBanner: false,
     );
   }
