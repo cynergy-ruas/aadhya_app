040e1b0f63797b73b26d54e43489d28b45f33e93
firekind <shyamant.achar@gmail.com>
2020-01-16 22:18:22 +0530

    fixed details page issue

--- lib/pages/details/page_header.dart
+++ lib/pages/details/page_header.dart
@@ -16,7 +16,6 @@ class PageHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      // width: MediaQuery.of(context).size.width * ,
       height: MediaQuery.of(context).size.height * 0.2,
       child: CustomMultiChildLayout(
         delegate: RelativeDelegate(objectCenter: FractionalOffset(0.5, 0)),
